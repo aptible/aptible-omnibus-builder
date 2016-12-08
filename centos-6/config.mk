574faca07b1d79d6a5a1099c663f7533d29b1ea5
Thomas Orozco <thomas@orozco.fr>
2016-12-08 12:48:25 +0100

    CentOS 6 Builder - Update repoforge package
    See:
    
    - https://github.com/repoforge/rpms/issues/375
    - https://github.com/repoforge/rpms/issues/377

--- centos-6/config.mk
+++ centos-6/config.mk
@@ -1,3 +1,3 @@
 export BASE_IMAGE = centos:6
 export PLATFORM = el
-export GIT_FROM_RPMFORGE_PKG = http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm
+export GIT_FROM_RPMFORGE_PKG = http://repository.it4i.cz/mirrors/repoforge/redhat/el6/en/x86_64/rpmforge/RPMS/rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm
