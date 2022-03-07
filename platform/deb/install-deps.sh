c7a05d6fca8a7c702b4d3d2a86e701dd4e9eb122
Eric Bower <me@erock.io>
2022-03-07 11:17:33 -0500

    using ubuntu 2004

--- platform/deb/install-deps.sh
+++ platform/deb/install-deps.sh
@@ -2,7 +2,6 @@
 set -o errexit
 set -o nounset
 
-apt-key update
 apt-get update
-apt-get install -y sudo wget git build-essential
+apt-get install -y sudo wget git build-essential cmake pkg-config gnupg
 rm -rf /var/lib/apt/lists/*
--- ubuntu-1404/config.mk
+++
@@ -1,2 +0,0 @@
-export BASE_IMAGE = ubuntu:14.04
-export PLATFORM = deb
