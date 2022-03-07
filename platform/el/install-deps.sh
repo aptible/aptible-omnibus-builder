c34bff375704fba0f2776dbedd369f34c6288a04
Eric Bower <me@erock.io>
2022-03-07 12:03:39 -0500

    build cmake for el platform

--- platform/el/install-deps.sh
+++ platform/el/install-deps.sh
@@ -7,9 +7,13 @@ yum install -y epel-release
 yum install -y wget sudo which fakeroot
 yum groupinstall -y "Development tools"
 
-if [[ -n "$GIT_FROM_RPMFORGE_PKG" ]]; then
-  rpm -i "$GIT_FROM_RPMFORGE_PKG"
-  yum --disablerepo=base,updates --enablerepo=rpmforge-extras -y install git
-fi
+# manually install cmake because the one provided by
+# yum is 2.x and we need >= 3.5.x
+wget https://cmake.org/files/v3.12/cmake-3.12.3.tar.gz
+tar zxvf cmake-3.*
+cd cmake-3.*
+./bootstrap --prefix=/usr/local
+make -j$(nproc)
+make install
 
 yum clean all
