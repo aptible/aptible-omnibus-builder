5441d7a8869c6d6976ef0503836eda01747a9ec4
Eric Bower <me@erock.io>
2022-03-07 11:32:29 -0500

    only support centos 7

--- centos-6/config.mk
+++
@@ -1,3 +0,0 @@
-export BASE_IMAGE = centos:6
-export PLATFORM = el
-export GIT_FROM_RPMFORGE_PKG = http://repository.it4i.cz/mirrors/repoforge/redhat/el6/en/x86_64/rpmforge/RPMS/rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm
